b4379c6c049361942fd856093d228bd431a293ba
dima1997 <franki.dimaria@gmail.com>
2024-02-28 23:57:08 +0100

    add: jl UCBArm update

--- julia/src/upper_confidence_bound_arm.jl
+++ julia/src/upper_confidence_bound_arm.jl
@@ -2,13 +2,13 @@ mutable struct UpperConfidenceBoundArm
     """
     https://fr.wikipedia.org/wiki/Algorithme_UCB
     """
-    c::Real # Confidence
-    μ::Real # Mean
-    N::Real # Number of pulls
+    confidence::Real    # Confidence
+    μ::Real             # Mean
+    N::Real             # Number of pulls
 end
 
-function UpperConfidenceBoundArm_init()
-    CumulativeAverageArm(0, 0)
+function UpperConfidenceBoundArm_init(confidence::Real)
+    UpperConfidenceBoundArm(confidence, 0, 0)
 end
 
 function pull(arm::UpperConfidenceBoundArm)
@@ -16,11 +16,11 @@ function pull(arm::UpperConfidenceBoundArm)
     if arm.N == 0
         n = 1
     end
-    arm.μ + arm.c * sqrt(log(n) / n)
+    arm.μ + arm.confidence * sqrt(log(n) / n)
 end
 
-# function update(arm::UpperConfidenceBoundArm, x::Real)
-#     arm.μ = (arm.N * arm.μ + x) / (arm.N + 1)
-#     arm.N += 1
-#     arm
-# end
+function update(arm::UpperConfidenceBoundArm, observation::Real)
+    arm.μ = (arm.N * arm.μ + observation) / (arm.N + 1)
+    arm.N += 1
+    arm
+end
--- julia/tests/test_upper_confidence_bound_arm.jl
+++ julia/tests/test_upper_confidence_bound_arm.jl
@@ -4,17 +4,18 @@ using Random
 include("../src/upper_confidence_bound_arm.jl")
 
 @testset "UpperConfidenceBoundArm Tests" begin
-    arm  = UpperConfidenceBoundArm_init()
+    confidence = 2
+    arm = UpperConfidenceBoundArm_init(confidence)
     prior_reward = pull(arm)
     @test prior_reward == 0.0
 
-    # observed_reward = 0.5
-    # update(arm, observed_reward)
-    # posterior_reward = pull(arm)
-    # @test posterior_reward == 0.5
+    observed_reward = 0.5
+    update(arm, observed_reward)
+    posterior_reward = pull(arm)
+    @test posterior_reward == 0.5
     
-    # observed_reward_2 = 0.75
-    # update(arm, observed_reward_2)
-    # posterior_reward_ = pull(arm)
-    # @test posterior_reward_ == 0.625
+    observed_reward_2 = 0.75
+    update(arm, observed_reward_2)
+    posterior_reward_2 = pull(arm)
+    @test posterior_reward_2 == 1.8024100225154747
 end;
