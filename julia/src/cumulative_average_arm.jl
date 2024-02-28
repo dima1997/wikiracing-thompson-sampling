0d6df2ca870f7d4cd8bb2b6949ef45a9e05b89a0
dima1997 <franki.dimaria@gmail.com>
2024-02-28 23:32:47 +0100

    add: jl CumulativeAverageArm

--- julia/src/cumulative_average_arm.jl
+++ julia/src/cumulative_average_arm.jl
@@ -12,3 +12,9 @@ end
 function pull(arm::CumulativeAverageArm)
     arm.μ
 end
+
+function update(arm::CumulativeAverageArm, x::Real)
+    arm.μ = (arm.N * arm.μ + x) / (arm.N + 1)
+    arm.N += 1
+    arm
+end
--- julia/tests/test_cumulative_average_arm.jl
+++ julia/tests/test_cumulative_average_arm.jl
@@ -5,6 +5,17 @@ include("../src/cumulative_average_arm.jl")
 
 @testset "CumulativeAverageArm Tests" begin
     arm  = CumulativeAverageArm_init()
-    @test pull(arm) == 0.0
+    prior_reward = pull(arm)
+    @test prior_reward == 0.0
+
+    observed_reward = 0.5
+    update(arm, observed_reward)
+    posterior_reward = pull(arm)
+    @test posterior_reward == 0.5
+    
+    observed_reward_2 = 0.75
+    update(arm, observed_reward_2)
+    posterior_reward_ = pull(arm)
+    @test posterior_reward_ == 0.625
 end;
 
